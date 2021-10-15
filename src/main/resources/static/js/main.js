feb1bc0fa9e2916b83c8edf1e559759830492d15
santos <ronireiosantos@gmail.com>
2021-10-16 01:54:01 +0300

    - rewrite edit urls on summary pages of orbeon

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,4 +3,20 @@ $(document).ready(function () {
         collapsible: true,
         heightStyle: "content"
     });
+
+    (function rewriteOrbeonSummaryPageUrls () {
+        // A hack to get around orbeon properties file where we are supposed to override the URLs not working,
+        const tableBodyElements = $(".orbeon table tbody");
+        if (tableBodyElements && tableBodyElements[0]) {
+            let tableBodyElement = tableBodyElements[0];
+            const clonedElement = tableBodyElement.cloneNode(true);
+            $(tableBodyElement).replaceWith(clonedElement);
+
+            $(".orbeon table tbody tr[class^=fr-summary-row] td span a[href^=\\/fr\\/]").each(function () {
+                let href = $(this).attr('href');
+                href = href.replace('/fr/', `${contextPath}/forms/`);
+                $(this).attr('href', href);
+            });
+        }
+    })()
 });
