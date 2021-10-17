5435ef61b91c6df466f7626c57a365da193535a3
santos <ronireiosantos@gmail.com>
2021-10-18 01:28:27 +0300

    - Fix form href not changed when searching is performed

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,19 +4,59 @@ $(document).ready(function () {
         heightStyle: "content"
     });
 
-    (function rewriteOrbeonSummaryPageUrls () {
-        // A hack to get around orbeon properties file where we are supposed to override the URLs not working,
-        const tableBodyElements = $(".orbeon table tbody");
-        if (tableBodyElements && tableBodyElements[0]) {
-            let tableBodyElement = tableBodyElements[0];
-            const clonedElement = tableBodyElement.cloneNode(true);
-            $(tableBodyElement).replaceWith(clonedElement);
+    const orbeonSummaryTableSelector = ".orbeon div.fr-mode-summary div.fr-summary-table-div table tbody";
+    let interval;
 
-            $(".orbeon table tbody tr[class^=fr-summary-row] td span a[href^=\\/fr\\/]").each(function () {
-                let href = $(this).attr('href');
-                href = href.replace('/fr/', `${contextPath}/forms/`);
-                $(this).attr('href', href);
-            });
+    const observeDOM = (function(){
+        const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+        return function( obj, callback ){
+            if( !obj || obj.nodeType !== 1 ) return;
+
+            if(MutationObserver){
+                // define a new observer
+                const mutationObserver = new MutationObserver(callback)
+                // have the observer observe foo for changes in children
+                mutationObserver.observe(obj, { childList:true, subtree:true })
+                return mutationObserver
+            }
+            // browser support fallback
+            else if(window.addEventListener){
+                obj.addEventListener('DOMNodeInserted', callback, false)
+                obj.addEventListener('DOMNodeRemoved', callback, false)
+            }
         }
     })()
+
+    function rewriteOrbeonSummaryPageUrls (element) {
+        if (!$(element)) {
+            clearInterval(interval);
+            return;
+        }
+        // A hack to get around orbeon properties file where we are supposed to override the URLs not working,
+        const clonedElement = element.cloneNode(true);
+        $(element).replaceWith(clonedElement);
+
+        $(`${orbeonSummaryTableSelector} tr[class^=fr-summary-row] td span a[href^=\\/fr\\/]`).each(function () {
+            let href = $(this).attr('href');
+            href = href.replace('/fr/', `${contextPath}/forms/`);
+            $(this).attr('href', href);
+        });
+
+        $(orbeonSummaryTableSelector).each(function () {
+            // Observe a specific DOM element:
+            observeDOM(this, (m) => rewriteOrbeonSummaryPageUrls(this));
+        })
+    }
+
+    const tableBodyElements = $(orbeonSummaryTableSelector);
+
+    if (tableBodyElements && tableBodyElements[0]) {
+        rewriteOrbeonSummaryPageUrls(tableBodyElements[0]);
+        // 1 second might be too small and can cause browser freezing,
+        // might want to give it some higher value in case it causes trouble,
+        // the interval was setup to solve the builder summary not working after search,
+        // even though the fr summary page was working
+        interval = setInterval(rewriteOrbeonSummaryPageUrls, 1000, tableBodyElements[0])
+    }
 });
