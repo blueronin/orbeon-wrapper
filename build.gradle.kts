f86e32b7ef62a0aff2edc1472309ddbd27c4c4c0
santos <ronireiosantos@gmail.com>
2022-01-10 21:05:50 +0300

    Disable spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,6 @@ dependencies {
     implementation("javax.servlet:jstl")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -32,7 +31,6 @@ dependencies {
     providedRuntime("mysql:mysql-connector-java")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
 }
 
 allOpen {
